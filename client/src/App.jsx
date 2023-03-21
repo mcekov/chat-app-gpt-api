4996c470bdcdc20f8f71ea774c08abfb2697805c
Momchil Tsekov <mtsekov@zohomail.eu>
2023-03-21 06:54:47 +0200

    app update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Chat from "@/components/chat";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </BrowserRouter>
